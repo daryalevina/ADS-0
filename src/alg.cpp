6e2a5d370bd3a4b7f23e9871d0f72cd6bd3aeb17
daryalevina <78952508+daryalevina@users.noreply.github.com>
2021-06-01 17:49:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,8 @@
 // Copyright 2021 NNTU-CS
 int gcd(int a, int b) {
-	while (a != b) {
-		if (a > b)
-			a = a - b;
-		else
-			b = b - a;
+	if (b == 0)
+		return a;
+	else {
+		return gcd(b, a % b);
 	}
-	return a; 
 } 
